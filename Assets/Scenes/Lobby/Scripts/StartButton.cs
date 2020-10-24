ddbb26447dc38b1af3ac0c49cb36602edf6f47e2
bbontrager89 <bbontrager89@gmail.com>
2020-10-24 16:26:30 -0700

    Start button scripts

---
+++ Assets/Scenes/Lobby/Scripts/StartButton.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class StartButton : MonoBehaviour {
+	public Button start;
+
+	void Start () {
+		Button btn = start.GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+	}
+
+	void TaskOnClick(){
+		Debug.Log("CLICKY BITCH");
+	}
+}
